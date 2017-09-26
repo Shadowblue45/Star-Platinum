9ca0dbcd2c4cd642fdec8d31ec4c3ea5a6b36191
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-09-26 11:44:26 -0400

    new stuff

---
+++ Fahadullah Asif/src/attendance/Student.java
@@ -0,0 +1,14 @@
+package attendance;
+
+public class Student implements Attendee {
+	
+	private boolean present = false;
+	
+	public boolean isPresent() {
+		return present;
+	}
+	
+	public void setPresent(boolean present) {
+		present = true;
+	}
+}
